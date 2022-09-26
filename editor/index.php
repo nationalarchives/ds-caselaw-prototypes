e34dc1709f77abaa42aef4c981756728d603de0a
Terry-Price <terry.price@nationalarchives.gov.uk>
2022-09-26 12:36:38 +0100

    main navigation idea for EUI

--- editor/index.php
+++ editor/index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Caselaw Editor UI frontend prototypes</title>
+    <p>There are currently no prototypes here yet.</p>
 </head>
 <body>
     <h1>Caselaw Editor UI prototypes</h1>
-    <p>There are currently no prototypes here yet.</p>
+    <ul>
+        <li><a href="/main-nav-idea/v-1-1/index.php">v1.0 – Main navigation idea</a></li>
+</ul>
+    
 </body>
 </html>
