ec3ddba5fccf7881f34da2b7c95cdb26cabb5edc
Rooha Sulehri <rooha.sulehri@nationalarchives.gov.uk>
2022-10-04 16:20:02 +0100

    add menu label and up/down chevrons

---
+++ editor/main-nav-idea/v1-0/index.php
@@ -0,0 +1,518 @@
+<!DOCTYPE html>
+<!-- saved from url=(0047)https://editor.caselaw.nationalarchives.gov.uk/ -->
+<html lang="en-gb"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="robots" content="noindex,nofollow">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Find and manage case law</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="https://editor.caselaw.nationalarchives.gov.uk/static/images/favicons/favicon.png">
+
+    
+      <link href="../v1-0/assets/css2" rel="stylesheet">
+      <link href="../v1-0/assets/main.css" rel="stylesheet">
+      <link href="../v1-0/assets/nav.css" rel="stylesheet">
+    
+
+    
+      <script defer="" src="../v1-0/assets/app.js"></script>
+    
+  </head>
+
+  <body>
+    <a id="skip-to-main-content" href="https://editor.caselaw.nationalarchives.gov.uk/#main-content">Skip to Main Content</a>
+    
+    
+    <main id="main-content">
+      
+    <header class="page-header">
+     
+
+<div class="page-header__breadcrumb">
+  <nav class="page-header__breadcrumb-container" aria-label="Breadcrumb">
+    <ol>
+      <li><a href="https://www.nationalarchives.gov.uk/">Home</a></li>
+      <li><a href="https://editor.caselaw.nationalarchives.gov.uk/" aria-current="page">Find and manage case law</a></li>
+      
+    </ol>
+  </nav>
+</div>
+
+     
+<div class="page-header__container">
+  <a class="page-header__logo-link" href="https://editor.caselaw.nationalarchives.gov.uk/">
+    <img class="page-header__logo" src="./Find and manage case law_files/tna_logo.svg" alt="The National Archives home" width="320" height="44">
+  </a>
+</div>
+
+    </header>
+
+    <div class="service-introduction">
+        <div class="service-introduction__container">
+            <span class="service-introduction__separator"></span>
+            <h1 class="service-introduction__header">Find and manage case law</h1>
+            <p class="service-introduction__helper-text">
+                Use this service to find and manage judgments and tribunal decisions on the 'Find Case Law' site<br>
+            </p>
+        </div>
+    </div>
+    <div class="search-component">
+  <div class="search-component__container">
+    <h2 class="search-component__header--sr-only">Search by keyword or neutral citation</h2>
+    <form action="https://editor.caselaw.nationalarchives.gov.uk/results" role="search" aria-label="Search by keyword or neutral citation" id="analytics-basic-search">
+
+      <label for="search_term" class="search-component__search-term-label"></label>
+      <input class="search-component__search-term-input" id="search_term" name="query" type="search" value="" placeholder="Enter a keyword or neutral citation">
+      
+      <input type="submit" value="Search">
+      <div>
+      </div>
+    </form>
+  </div>
+</div>
+
+
+<!-- START Navigation bar -->
+
+<div class="main-nav">  
+  <div>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn">
+      Menu 
+      <span class="nav-icon"></span>
+    </label>
+    <ul class="menu">
+      <li><a href="https://www.bbc.co.uk">Incoming judgments</a></li>
+      <li><a href="https://www.bbc.co.uk">Recently published judgments</a></li>
+      <li><a href="https://www.bbc.co.uk">Deleted judgments</a></li>
+    </ul>
+  </div>
+  <div class="actions">
+    <ul>
+      <li class="search"><a href="https://www.bbc.co.uk">Search</a></li>
+      <li class="logout"><a href="https://www.bbc.co.uk">Log out</a></li>
+    </ul>
+  </div>
+</div>
+
+<!-- END Navigation bar -->
+
+<div class="recent-judgments">
+  <div class="recent-judgments__container">
+    <h2 id="recently-published-judgments" class="recent-judgments__header">Unpublished judgments</h2>
+    <ul class="recent-judgments__list">
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewhc/fam/2022/2250">
+                
+                  Guy’s and St Thomas’ NHS Foundation Trust v A &amp; Ors
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              [2022] EWHC 2250 (Fam)
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: 2022-08-26
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Beronique Addington
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: beronique.addington@justice.gov.uk
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 26 Aug 2022, 4:36 p.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-GFQ
+            </span>
+          </span>
+        </li>
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewhc/pat/2022/2034">
+                
+                  SHENZHEN CARKU TECHNOLOGY CO, LTD v THE NOCO COMPANY
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              [2022] EWHC 2034 (Pat)
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: 2022-08-04
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Pauline Drewett
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: pauline.drewett@justice.gov.uk
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 4 Aug 2022, 9:35 a.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-FVW
+            </span>
+          </span>
+        </li>
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewhc/ch/2022/2211">
+                
+                  Meadow Designs Limited &amp; Ors. v Rishco Leisure Limited &amp; Anor.
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              [2022] EWHC 2211 (Ch)
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: 2022-07-28
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Nichola Pierce
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: nichola.pierce@justice.gov.uk
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 24 Aug 2022, 10:57 a.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-GCQ
+            </span>
+          </span>
+        </li>
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewhc/scco/2022/2230">
+                
+                  Regina v Dale
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              None
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: None
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Felice Risveglia
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: felice.risveglia@ejudiciary.net
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 24 Aug 2022, 3:11 p.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-GC3
+            </span>
+          </span>
+        </li>
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewhc/qb/2022/1816">
+                
+                  Murtaza Ali Shah v Ajaz Ahmed
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              [2022] EWHC 1816 (QB)
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: 2022-07-13
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Nizanka Yoganathan
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: nizanka.yoganathan@justice.gov.uk
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 13 Jul 2022, 12:05 p.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-D4R
+            </span>
+          </span>
+        </li>
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewca/crim/2022/953">
+                
+                  [UNPUBLISHED] REGINA v AYODEJI OJUROYE
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              [2022] EWCA Crim 953
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: 2022-07-12
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Susan Jenkins
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: susan.jenkins4@justice.gov.uk
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 12 Jul 2022, 9:03 a.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-D36
+            </span>
+          </span>
+        </li>
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewhc/scco/2022/1775">
+                
+                  REGINA v BARZEY
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              None
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: 2022-06-30
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Felice Risveglia
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: felice.risveglia@ejudiciary.net
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 8 Jul 2022, 1:53 p.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-D2N
+            </span>
+          </span>
+        </li>
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewhc/ipec/2022/2197">
+                
+                  Vernacare Limited v Moulded Fibre Products Limited
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              None
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: 2022-06-15
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Rachel Morgan
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: rachel.morgan3@justice.gov.uk
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 1 Sep 2022, 12:31 p.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-GGS
+            </span>
+          </span>
+        </li>
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewhc/admin/2022/3486">
+                
+                  R (PROFESSOR PAUL TAGGART) v THE ROYAL COLLEGE OF SURGEONS OF ENGLAND
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              [2022] EWHC 3486 (Admin)
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: 2022-05-13
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Jacob Scarry
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: jacob.scarry@justice.gov.uk
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 27 Jul 2022, 2:06 p.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-FMX
+            </span>
+          </span>
+        </li>
+      
+        <li class="recent-judgments__judgment">
+          <span>
+            <span class="recent-judgments__title">
+              <a href="https://editor.caselaw.nationalarchives.gov.uk/detail?judgment_uri=ewca/crim/2022/614">
+                
+                  [DO NOT PUBLISH - ANONYMITY ISSUE OUTSTANDING] REGINA v FLORIAN PETRICA CALIN
+                
+              </a>
+            </span>
+            <span class="recent-judgments__neutralcitation">
+              [2022] EWCA Crim 614
+            </span>
+          </span>
+          <span>
+            <span class="recent-judgments__date">
+              Date: 2022-04-28
+            </span>
+            <span class="recent-judgments__court">
+              Court: None
+            </span>
+             <span class="recent-judgments__submitter">
+              Submitted by: Simon Smith
+            </span>
+            <span class="recent-judgments__submitteremail">
+              Contact email: simon.smith@justice.gov.uk
+            </span>
+            <span class="recent-judgments__submissiondate">
+              Submission date: 5 May 2022, 8:39 a.m.
+            </span>
+            <span class="recent-judgments__consignmentrefence">
+              Consignment Reference: TDR-2022-SN
+            </span>
+          </span>
+        </li>
+      
+    </ul>
+    <nav class="pagination" aria-label="Results pagination">
+  <h3>Results pagination</h3>
+  <ul class="pagination__list">
+    <li class="pagination__list-item">
+      
+        <span class="pagination__page-chevron-previous--disabled">
+          <span>Previous page</span>
+      </span>
+      
+    </li>
+    <li class="pagination__list-pages">
+      <ol>
+        <li class="pagination__list-item">
+          <a class="pagination__page-link-current" href="https://editor.caselaw.nationalarchives.gov.uk/?page=1" aria-label="Current page, Page 1" aria-current="true">
+              <span>Current page </span>1
+          </a>
+        </li>
+        
+          <li class="pagination__list-item">
+            <a class="pagination__page-link" href="https://editor.caselaw.nationalarchives.gov.uk/?page=2" aria-label="Go to page 2">
+              <span>Page </span>2
+            </a>
+          </li>
+        
+          <li class="pagination__list-item">
+            <a class="pagination__page-link" href="https://editor.caselaw.nationalarchives.gov.uk/?page=3" aria-label="Go to page 3">
+              <span>Page </span>3
+            </a>
+          </li>
+        
+      </ol>
+    </li>
+    <li class="pagination__list-item">
+      
+        <a class="pagination__page-chevron-next" href="https://editor.caselaw.nationalarchives.gov.uk/?&amp;page=2">
+          <span>Next page</span>
+      </a>
+      
+    </li>
+  </ul>
+</nav>
+
+    <p class="recent-judgments__more-info"><a href="https://editor.caselaw.nationalarchives.gov.uk/results">See all recent judgments</a></p>
+  </div>
+</div>
+
+
+    </main>
+    <footer class="judgments-footer">
+  <div class="judgments-footer__container">
+    <h2 class="judgments-footer__heading">Footer</h2>
+    <ul class="judgments-footer__links">
+      <li>
+        All content is available under the Open Justice Licence, except where otherwise stated.
+      </li>
+    </ul>
+  </div>
+</footer>
+
+  
+
+</body></html>
